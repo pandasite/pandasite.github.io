565365eaa22c68b438a9c5b22cb368ad317e30ef
irwtmk <irwtmk@gmail.com>
2023-07-25 21:12:30 +0900

    update

---
+++ js/blank.js
@@ -0,0 +1,9 @@
+var links = document.links;
+
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+    if (links[i].hostname != window.location.hostname) {
+        links[i].target = "_blank";
+        links[i].rel = "noopener noreferrer";
+        links[i].classList.add("cursor-newtab");
+    }
+}
